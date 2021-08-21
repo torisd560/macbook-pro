491cbe2c535cc83635f6e4f70fe19e3eb20ec839
torisd560 <mitalisd123@gmail.com>
2021-08-21 10:12:53 +0600

    added updates

--- mac.js
+++ mac.js
@@ -1,49 +1,83 @@
 // memory cost
 document.getElementById('memory-8GB').addEventListener('click', function(){
     updateProductCost('memory', 0)
-    totalPrice()
 });
 document.getElementById('memory-16GB').addEventListener('click',function(){
     updateProductCost('memory', 180)
-    totalPrice()
 });
 // storage cost
 document.getElementById('storage-256').addEventListener('click',function(){
     updateProductCost('storage', 0)
-    totalPrice()
 });
 document.getElementById('storage-512').addEventListener('click',function(){
     updateProductCost('storage', 100)
-    totalPrice()
 });
 document.getElementById('storage-1TB').addEventListener('click',function(){
     updateProductCost('storage', 180)
-    totalPrice()
 });
 // delivery charge cost
 document.getElementById('free-delivery').addEventListener('click',function(){
     updateProductCost('delivery', 0)
-    totalPrice()
 });
 document.getElementById('charged-delivery').addEventListener('click',function(){
     updateProductCost('delivery', 20)
-    totalPrice()
 });
+
 // update product cost use function
 function updateProductCost(product ,cost){
     const previousProductCost= document.getElementById( product + '-cost');
     previousProductCost.innerText = cost;
-    return cost;
-    
-}
-function totalPrice(){
-    const bestPrice =  document.getElementById('best-price');
-    const updateBestPrice = Number(bestPrice.innerText) ;
+
+    // const memoryCost = document.getElementById('memory-cost').innerText ;
+    // const storagCost = document.getElementById('storage-cost').innerText;
+    // const deliveryCost = document.getElementById('delivery-cost').innerText;
+    const bestPrice = document.getElementById('best-price').innerText;
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = updateBestPrice + updateProductCost() ;
-    console.log(totalPrice)
+    totalPrice.innerText = Number(bestPrice) + getCost('memory') +  getCost('storage') +  getCost('delivery');
 }
-         
+// get product cost
+function getCost(type){
+    const typeCost = document.getElementById( type +'-cost') ;
+    const typeCostText = Number(typeCost.innerText);
+    return typeCostText ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function totalPrice(){
+//     getPrice('best')
+//     getPrice('total')
+// }
+//      function getPrice(price){
+//         const productPrice =  document.getElementById( price +'-price');
+//         const updateProductPrice = productPrice.innerText;
+//         productPrice.innerText = Number(updateProductPrice) + (updateProductCost());
+//      }    
 
 
 
